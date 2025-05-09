b4304f1fef5e2a5f9b6cf34bdcce1c97a23f6ca4
nicolasLuna1024 <nicolas.luna@epn.edu.ec>
2025-05-08 19:50:12 -0500

    Primer commit del proyecto FotoFlash

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab3',
@@ -6,8 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss'],
   standalone: false,
 })
+
 export class Tab3Page {
+  hasLoaded = false;
 
-  constructor() {}
 
+  constructor(public photoService: PhotoService) {}
+  async loadSaved() {
+    await this.photoService.loadSaved();
+    this.hasLoaded = true;
+  }
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
 }
